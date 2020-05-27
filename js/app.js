2d577c09fdbf44585e72d4b1f73d1d1de1a36c16
ashirbad behera <ashirbadbehera29@gmail.com>
2020-05-27 10:25:42 +0530

    formatted the code

--- js/app.js
+++ js/app.js
@@ -3,24 +3,32 @@ document.addEventListener('DOMContentLoaded', function () {
 	var elems = document.querySelectorAll('.modal');
 	var instances = M.Modal.init(elems);
 });
+
 //Initializing the Walpapers class
 const walpapers = new Walpapers();
 
 //caching the dom
 const walpaper_section = document.querySelector('.walpapers');
 
+//The Get request
 walpapers.getWalpapers().then(Response => {
 	show(Response);
 });
 
+/*
+ * function to pusblish the Walpaper
+ */
 function show(response) {
 	let output = '';
+
 	response.forEach(walpaper => {
-		//walpaper.data
+		// * Avoiding Any unnecessary posts without image
 		if (walpaper.data.thumbnail === 'self') {
 			return;
 		}
 
+		// * checking if the link is from reddit...
+		// * Sometimes the links are from other sites which can cause problems
 		if (walpaper.data.url.includes('i.redd.it')) {
 			output += `
 			<div class="img-container col s12 m12 l6">
@@ -29,6 +37,7 @@ function show(response) {
 				<a class="btn waves-effect btn-small" href="${walpaper.data.url}" download><i class="material-icons small">file_download</i></a>
 			</div>
 			`;
+			//* OverWriting the HTML whenever a new walpaper loades
 			walpaper_section.innerHTML = output;
 		}
 	});
